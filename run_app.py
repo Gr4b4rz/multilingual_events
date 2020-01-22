2cb9ca88752c5728401583751893cc3a3dd6baa4
Gr4b4rz <piotr.grabarski123@gmail.com>
2020-01-22 19:43:49 +0100

    Improve formatting and add some small fixes to python app

--- run_app.py
+++ run_app.py
@@ -14,10 +14,12 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("langs", help="Specify languages you want to compare delimated with"
                         "comas", type=str)
-    parser.add_argument("title", help="specify the title you want to search for", type=str)
+    parser.add_argument(
+        "title", help="specify the title you want to search for", type=str)
     args = parser.parse_args()
 
     return (args.langs.split(","), args.title)
+
 
 def get_full_english_ln_form(ln):
     full_form = ""
@@ -35,29 +37,37 @@ def get_full_english_ln_form(ln):
         full_form = "spanish"
     elif (ln == 'pt'):
         full_form = "portuguese"
+    elif (ln == 'nl'):
+        full_form = "norwegian"
 
     return full_form
+
 
 def search_for_keywords(content, ln, keywords):
     keyword_hits = []
     translator = Translator()
     stemmer = None
     if (ln == 'pl'):
-        for keyword in keywords:
+        f_pl = open('keywords_pl.txt', 'r')
+        keywords_pl = [keyword.strip() for keyword in f_pl]
+        f_pl.close()
+        for keyword in keywords_pl:
             hits_nb = content.count(" {}".format(keyword))
             if hits_nb > 0:
                 keyword_hits.append((keyword, hits_nb))
     else:
         translated_keywords = []
-        stemmer = SnowballStemmer(get_full_english_ln_form(ln), ignore_stopwords=True)
+        stemmer = SnowballStemmer(
+            get_full_english_ln_form(ln), ignore_stopwords=True)
         try:
             with open('keywords_{}.txt'.format(ln), 'r') as f:
                 translated_keywords = [keyword.strip() for keyword in f]
         except FileNotFoundError:
             translator = Translator()
-            translated_keywords = translator.translate(keywords, dest=ln, src='pl')
+            translated_keywords = translator.translate(
+                keywords, dest=ln, src='pl')
             translated_keywords = [stemmer.stem(keyword.text.lower()) if keyword.text.count(' ') > 0
-                                  else keyword.text.lower() for keyword in translated_keywords]
+                                   else keyword.text.lower() for keyword in translated_keywords]
             translated_keywords = list(dict.fromkeys(translated_keywords))
 
             print("Creating keywords_{}.txt file".format(ln))
@@ -73,10 +83,6 @@ def search_for_keywords(content, ln, keywords):
 
     return keyword_hits
 
-def stem_polish_keywords(keywords):
-    return keywords
-
-
 
 def print_result(keyword_hits, ln):
     print("*** {} ***".format(ln))
@@ -84,6 +90,7 @@ def print_result(keyword_hits, ln):
     for keyword in keyword_hits:
         table.add_row([keyword[0], keyword[1]])
     print(table)
+
 
 def create_ln_dict(keyword_hits, ctx_length, ln):
     number_of_hit_keywords = len(keyword_hits)
@@ -96,8 +103,10 @@ def create_ln_dict(keyword_hits, ctx_length, ln):
                "total_keyword_hits": number_of_total_hits, "keywords": dict(keyword_hits)}
     return ln_dict
 
+
 def write_to_db(es, es_document):
     es.index(index="processed-data", body=es_document)
+
 
 def main():
     nltk.download('stopwords')
@@ -119,6 +128,7 @@ def main():
     for ln in languages:
         content = ""
         wikipedia.set_lang(ln)
+        keyword_hits = None
         if (ln == 'pl'):
             content = wikipedia.page(title=title).content.lower()
             keyword_hits = search_for_keywords(content, ln, keywords)
@@ -129,14 +139,18 @@ def main():
                 if phrase in link:
                     x = len("https://.wikipedia.org/wiki/") + len(ln)
                     foreign_title = link[x:None]
-            content = wikipedia.page(title=foreign_title).content.lower()
+            try:
+                content = wikipedia.page(title=foreign_title).content.lower()
+                keyword_hits = search_for_keywords(content, ln, keywords)
+            except:
+                continue
 
-        keyword_hits = search_for_keywords(content, ln, keywords)
         print_result(keyword_hits, ln)
         ln_dict = create_ln_dict(keyword_hits, len(content), ln)
         es_document['keywords_info'].append(ln_dict)
 
     write_to_db(es, es_document)
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     main()
